71483fdbe9b4b171a633d64d71cde3f4c2a10483
ahfasxp <ahfas.fazria@gmail.com>
2026-07-03 12:43:39 +0700

    Add Invitable project to portfolio

--- data/projects.ts
+++ data/projects.ts
@@ -132,4 +132,10 @@ export const projects: Project[] = [
     image: "https://firebasestorage.googleapis.com/v0/b/promopreview/o/thumbnails%2Ffa0dfe8a-99ec-4421-8f63-14e5f2051ea1.png?alt=media&token=65117ad8-5405-422a-8bc2-60b829d191ae",
     type: "mobile",
   },
+  {
+    title: "Invitable",
+    tech: "Next.js, TypeScript, Supabase, Tailwind",
+    image: "https://firebasestorage.googleapis.com/v0/b/promopreview/o/thumbnails%2F3891eee6-5181-4ecb-8e48-3179cee8cee3.png?alt=media&token=e073d30e-b147-459b-b801-0c6b4940abe7",
+    type: "website",
+  },
 ];
